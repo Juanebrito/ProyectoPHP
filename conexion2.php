b8d35c2dca3bdb179bbca5eb93aac2dd68e6eaae
Juan Brito <juanebrito3.1@gmail.com>
2019-10-29 21:05:29 -0400

    vwrsion 1

---
+++ conexion2.php
@@ -0,0 +1,12 @@
+<?php
+
+function getConexion(){
+    try {
+        $pdo = new \PDO("mysql:host=localhost;dbname=Escuela", "root", "password");
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    }catch (PDOException $e){
+        print_r($e);
+        return null;
+    }
+}
